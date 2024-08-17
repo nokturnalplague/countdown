9453ed1a799b9ab3eec8cbd6c57d6b4cf9a5679d
Shisaki <99548587+nokturnalplague@users.noreply.github.com>
2024-08-17 12:53:47 -0300

    time to start function set to 0

--- src/script.js
+++ src/script.js
@@ -1,15 +1,13 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Sep 12, 2024 20:30:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// Função que faz o cálculo e exibe o contador
+function updateCounter() {
     // Get today's date and time
     var now = new Date().getTime();
-        
+    
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-    
-    console.log("distance");
 
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -22,7 +20,7 @@ var x = setInterval(function() {
     var minutesFormat = minutes <= 9 ? "0" + minutes : minutes;
     var secondsFormat = seconds <= 9 ? "0" + seconds : seconds;
 
-    // Output the result in an element with id="demo"
+    // Output the result in an element with id="counter"
     document.getElementById("counter").innerHTML = daysFormat + ":" + hoursFormat + ":" + minutesFormat + ":" + secondsFormat;
         
     // If the count down is over, write some text 
@@ -30,4 +28,10 @@ var x = setInterval(function() {
         clearInterval(x);
         document.getElementById("counter").innerHTML = "00:00:00:00";
     }
-}, 1000);
+}
+
+// Executa a função imediatamente
+updateCounter();
+
+// Atualiza o contador a cada 1 segundo
+var x = setInterval(updateCounter, 1000);
